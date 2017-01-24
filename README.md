33ea279721559ffe76ee51ae967a12040a9b8324
nryn <narayanohanlon@gmail.com>
2017-01-24 00:04:55 +0000

    refactored directory structure and updated shell script directory templater to match, README edits

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #Learn Ruby The Hard Way
 
 A repo for me to track progress while [learning Ruby the hard way](https://learnrubythehardway.org).
+
+`./initialExerciseTemplateCreator.sh 40 1` creates 40 chapter directories each with a single file inside.
--- initialExerciseTemplateCreation.sh
+++ initialExerciseTemplateCreation.sh
@@ -3,9 +3,9 @@
 # Pass two args, $1 = number of directories you want, $2 = number of files in each directory.
 
 for ((dircount = 1; dircount <= $1; dircount++)) do
-	mkdir "chap${dircount}"
+	mkdir "./lrthw/chap${dircount}"
 	for ((filecount = 1; filecount <= $2; filecount++))
 	do
-		touch "./chap${dircount}/ex${filecount}.rb"
+		touch "./lrthw/chap${dircount}/ex${filecount}.rb"
 	done
 done
